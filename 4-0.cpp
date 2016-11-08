2bb50c3405d86c52d2ebb98366a2f371e7f4a2f2
Timur <timur.savko@gmail.com>
2016-11-08 21:15:23 +0300

    Add files via upload

---
+++ 4-0.cpp
@@ -0,0 +1,20 @@
+﻿#include "iostream"
+#include "iomanip" 
+#include "cmath"
+
+using namespace std;
+
+#define PI 3.14 
+
+int main()
+{
+	float coordinatex = -6, coordinatey;
+	while (coordinatex <= 6)
+	{
+		if (coordinatex == 0) coordinatex = coordinatex + 5;
+		coordinatey = (sin(2*coordinatex ) / coordinatex)*20; coordinatex = coordinatex + 0.1;
+		cout << setw(40 + (coordinatey)) << setfill(' ') << '*' << "\n";
+
+	}
+	system("pause");
+}
